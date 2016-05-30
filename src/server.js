647aaf0ac6079e37f06f13d11d5af74c2c4054dd
hsouth95 <hsouth95@hotmail.co.uk>
2016-05-30 16:49:14 +0100

    Test of auto deploy

--- src/server.js
+++ src/server.js
@@ -8,7 +8,8 @@ const PORT = 80;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('This was pushed by Github!\n');
+  var test = 1 + 1;
+  res.send('This was pushed by Github!\n' + 'This is a sum:' + test);
 });
 
 app.listen(PORT);
