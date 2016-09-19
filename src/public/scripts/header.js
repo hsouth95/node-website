4258d0d2c75f17dc5e5dac3c36b9ccb56ce8b7da
hsouth95 <hsouth95@hotmail.co.uk>
2016-09-19 20:11:34 +0100

    Create a sticky nav bar for #2

---
+++ src/public/scripts/header.js
@@ -0,0 +1,19 @@
+(function(){
+    var navBarHeight = $("header").outerHeight(true);
+
+    /**
+     * Shrinks the navigation bar and makes it sticky so it moves down with the page
+     * @param barHeight - Height of the bar
+     */
+    function shrinkNavigationBar(barHeight){
+        if($(document).scrollTop() > (barHeight / 2)) {
+            $("header").addClass("shrink");
+        } else {
+            $("header").removeClass("shrink");
+        }
+    }
+
+    $(window).scroll(function(){
+        shrinkNavigationBar(navBarHeight);
+    });
+}());
